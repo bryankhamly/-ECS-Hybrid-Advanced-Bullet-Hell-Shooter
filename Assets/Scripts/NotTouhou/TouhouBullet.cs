2f968857b60d10ad0abcd822451760cb132dc84f
Bryan <bryankhamly@gmail.com>
2018-05-18 08:42:54 -0700

    Starting Touhou system for enemies.
    Specialized Bullets, will need patterns later

---
+++ Assets/Scripts/NotTouhou/TouhouBullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouhouBullet : MonoBehaviour
+{
+	public float angle;
+	public float speed;
+
+	public void Initialize(float angle, float speed) //Too bad can't use a constructor on MonoBehaviours
+	{
+		this.angle = angle;
+		this.speed = speed;
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, angle);
+	}
+}
