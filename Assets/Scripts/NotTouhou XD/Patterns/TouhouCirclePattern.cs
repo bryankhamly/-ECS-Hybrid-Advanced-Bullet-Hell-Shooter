c0e8c9ed89431b7c58034dc7d6ecee547aa2afb6
Bryan <bryankhamly@gmail.com>
2018-05-18 11:05:36 -0700

    Circle Pattern

---
+++ Assets/Scripts/NotTouhou XD/Patterns/TouhouCirclePattern.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouhouCirclePattern : TouhouPattern
+{
+    public override void ShootBullet ()
+    {
+        float angleOffset = 360f / (float) bulletsToShoot;
+
+		 for (int i = 0; i < bulletsToShoot; i++)
+        {
+            var bullet = CreateBullet(transform.position, transform.rotation);
+            float angle = angleOffset * i;
+
+            InitBullet(this, bullet, angle, bulletSpeed);  
+        }
+
+		available = true;
+    }
+}
