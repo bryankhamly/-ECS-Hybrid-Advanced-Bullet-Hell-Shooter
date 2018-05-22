5b07d379c1bab9b892953369a83458cc575277ed
Bryan <bryankhamly@gmail.com>
2018-05-22 10:24:23 -0700

    Half Circles, and Aiming

--- Assets/Scripts/NotTouhou XD/Patterns/TouhouCirclePattern.cs
+++ Assets/Scripts/NotTouhou XD/Patterns/TouhouCirclePattern.cs
@@ -4,18 +4,78 @@ using UnityEngine;
 
 public class TouhouCirclePattern : TouhouPattern
 {
+    [Header ("Half Circles")]
+    public bool useHalfCircle;
+    [Range (0f, 360f)]
+    public float aimAngle = 90f;
+    public bool autoAim;
+    public Transform target;
+
     public override void ShootBullet ()
     {
-        float angleOffset = 360f / (float) bulletsToShoot;
-
-		 for (int i = 0; i < bulletsToShoot; i++)
+        if (autoAim)
         {
-            var bullet = CreateBullet(transform.position, transform.rotation);
-            float angle = angleOffset * i;
-
-            InitBullet(this, bullet, angle, bulletSpeed, bulletAccel, bulletDamage, bulletStrafe);  
+            LookAtTarget ();
         }
 
-		available = true;
+        float angleOffset;
+
+        if (useHalfCircle)
+        {
+            angleOffset = 180f / (float) bulletsToShoot;
+        }
+        else
+        {
+            angleOffset = 360f / (float) bulletsToShoot;
+        }
+
+        for (int i = 0; i < bulletsToShoot; i++)
+        {
+            var bullet = CreateBullet (transform.position, transform.rotation);
+
+            float angle;
+
+            if (useHalfCircle)
+            {
+                angle = aimAngle + angleOffset * i;
+            }
+            else
+            {
+                angle = angleOffset * i;
+            }
+
+            InitBullet (this, bullet, angle, bulletSpeed, bulletAccel, bulletDamage, bulletStrafe);
+        }
+
+        available = true;
+
+        if (autoAim)
+        {
+            LookAtTarget ();
+        }
+    }
+
+    void LookAtTarget ()
+    {
+        if (target)
+        {
+            aimAngle = TrigStuff.CalculateZAngleDifference (transform.position, target.transform.position) - 90f;
+        }
+    }
+
+    IEnumerator Aim ()
+    {
+        while (autoAim)
+        {
+            if (available)
+            {
+                yield break;
+            }
+
+            LookAtTarget ();
+
+            yield return 0;
+        }
+        yield break;
     }
 }
