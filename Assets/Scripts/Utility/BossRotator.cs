02fca2e06e051fce053194500c79c6295ea1df2a
Bryan <bryankhamly@gmail.com>
2018-05-31 17:42:05 -0700

    kikikikiki

---
+++ Assets/Scripts/Utility/BossRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRotator : MonoBehaviour
+{
+	public float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate(0,0, speed * Time.deltaTime);
+	}
+}
